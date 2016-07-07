3af708df8728e8257786191e75241c1b8151cfc6
sandro_alex <sandro.og@gmail.com>
2016-07-07 04:05:39 +0000

    Exp los módulos en formarto json

---
+++ tutorial/9_mas_modules/9_modules.js
@@ -0,0 +1,5 @@
+//'./pelis' hace referencia al archivo pelis.js
+// ./ indica el mismo directorio
+var peliculas = require('./pelis');
+peliculas.imprimirAvatar();
+console.log(peliculas.PeliFav);
---
+++ tutorial/9_mas_modules/pelis.js
@@ -0,0 +1,20 @@
+// ESTO ES LO  mismo, pero se pone solo lo que queremos exportar
+
+module.exports = {
+    imprimirAvatar: function(){
+        console.log("Avatar : PG-13....");
+    },
+    imprimirUp: function(){
+        console.log("Up : R....");
+    },
+    PeliFav: "The Matix"
+};
+
+// function imprimirAvatar(){
+//     console.log("Avatar : PG-13");
+// }
+// function imprimirUp(){
+//     console.log("Up : R");
+// }
+//module.exports.avatar = imprimirAvatar;
+//module.exports.Up = imprimirUp;
