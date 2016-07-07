e4238e386a4069eb463362aab24887ffb814e57a
sandro_alex <sandro.og@gmail.com>
2016-07-07 02:36:46 +0000

    Añadiendo funciones y propiedades a los objetos

--- tutorial/7_Prototype.js
+++ tutorial/7_Prototype.js
@@ -16,3 +16,16 @@ Malena.nombre = "Malena";
 Sandro.Davida(Malena);
 console.log("Sandro: " + Sandro.vida )
 console.log("Malena: " + Malena.vida )
+//Esto es lo nuevo, se puede añadir   funciones a todos los objetos
+Usuario.prototype.quitaVida = function uppercut(jugador){
+        jugador.vida -= 5;
+        console.log(this.nombre + " quitó 5 vidas a " + jugador.nombre);
+}
+Malena.quitaVida(Sandro);
+console.log("Sandro: " + Sandro.vida );
+console.log("Malena: " + Malena.vida );
+
+//se puede añadir propiedades   a todos los objetos
+Usuario.prototype.magia = 120;
+console.log("Sandro.magia: " + Sandro.magia );
+console.log("Malena.magia: " + Malena.magia );
